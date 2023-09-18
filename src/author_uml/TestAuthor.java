c904061e2948167c4bbe3868854ef4cd434dc251
Lika <135517028+LiAngelic@users.noreply.github.com>
2023-09-18 18:06:17 +0300

    TestAuthor.java

--- src/author_uml/TestAuthor.java
+++ src/author_uml/TestAuthor.java
@@ -1,2 +1,15 @@
-package author_uml;public class TestAuthor {
+package author_uml;
+import java.lang.*;
+import java.util.Scanner;
+public class TestAuthor {
+    public static void main(String[] args) {
+        String email;
+        Author lika = new Author("Лика", "lika.eriomenko@gmail.com", 'ж' );
+        System.out.println("\nemail: " + lika.getEmail());
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nВведите email: ");
+        email = input.next();
+        lika.setEmail(email);
+        System.out.println("\nНовый email: " + lika.getEmail());
+    }
 }
